447c66e26e17e50400199492e4c56b0794843fdd
Rugile Karengaite <rugile.karengaite@ktu.edu>
2021-08-31 12:41:29 +0300

    broken homepage to smaller components and finished styling our directory component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
-import './homepage.styles.scss'
+import Homepage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (

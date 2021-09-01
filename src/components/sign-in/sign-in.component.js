2ae9a262943e56da08f38e834bad50eedf90e97b
Rugile Karengaite <rugile.karengaite@ktu.edu>
2021-09-01 16:42:33 +0300

    finished sign up and sing in

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 
 class SingIn extends React.Component {
     constructor(props){
@@ -14,9 +14,16 @@ class SingIn extends React.Component {
             password: ''
         }
     }
-    handleSubmit= event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({email: '', password: ''})
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''})
+        }
+        catch(err){
+            console.log(err)
+        }
     }
     handleChange = event => {
         const {value, name} = event.target;

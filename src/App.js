0b0167dd9de80238d785eedec86cf84a82a7bd97
Rugile Karengaite <rugile.karengaite@ktu.edu>
2021-09-01 11:12:32 +0300

    created header

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
 import './pages/homepage/homepage.styles.scss'
 
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact path='/' component={Homepage}></Route>
       <Route exact path='/shop' component={ShopPage}></Route>
